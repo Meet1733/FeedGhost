e76521aaa61e981fb419847e95c2a3939f0e018b
MEET1733 <meetharsoda5@gmail.com>
2024-11-23 16:28:12 +0530

    Eslint-error-solved-2

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -10,7 +10,7 @@ import { useRouter } from "next/navigation"
 import { signUpSchema } from "@/schemas/signUpSchema"
 import axios , {AxiosError} from 'axios'
 import { ApiResponse } from "@/types/ApiResponse"
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import { Loader2 } from "lucide-react"
@@ -70,7 +70,7 @@ const SignIn = () => {
         console.error("Error in signup of user", error)
         const axiosError = error as AxiosError<ApiResponse>;
 
-        let errorMessage = axiosError.response?.data.message
+        const errorMessage = axiosError.response?.data.message
         toast({
           title: "Signup failed",
           description: errorMessage,
